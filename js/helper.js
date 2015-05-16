792a023f42e362083c8e5adc360b9bbd9852c707
Frank Steidinger <steidinger@acm.org>
2015-05-16 14:25:02 +0200

    adds info window for map markers

--- js/helper.js
+++ js/helper.js
@@ -85,7 +85,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.clientX, loc.clientY);
 });
 
 
@@ -170,7 +170,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
